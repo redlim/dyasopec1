3cfdd203e84061959591749d39da5f2d514bc37d
Will <wilder.olmos@corusconsulting.com>
2023-11-11 12:28:23 +0100

    Updating process and adding the first version of help

--- DyASO_PED1_Olmos_Bernal_Wilder/Trabajo1/Demonio.sh
+++ DyASO_PED1_Olmos_Bernal_Wilder/Trabajo1/Demonio.sh
@@ -1,14 +1,49 @@
 #!/bin/bash
-
+#declaración de constantes
+processFile="procesos";
+serviceProcessFile="procesos_servicio";
+intervalProcessFile="procesos_periodicos";
+devilProcessFile="procesos_demonio";
+bibleFile="Biblia.txt";
+apocalipisFile="Apocalipsis";
+saintPeterFile="SanPedro";
+files=($processFile $serviceProcessFile $intervalProcessFile $devilProcessFile $bibleFile $apocalipisFile $saintPeterFile);
+filesProcess=($processFile $serviceProcessFile $intervalProcessFile);
+hellDirectory="Infierno"
 #Demonio Dummie, tenéis que completarlo para que haga algo
 
+function writeBible() {
+    date=$(date +%H:%M:%S);
+    echo "${date} $1" >> $bibleFile;
+}
+
+finishProcess() {
+    echo "finalizando procesos"
+    pid="el pid a finalizar"
+    writeBible "El proceso $pid ha terminado"
+    writeBible "se acabó el mundo"
+}
+
+deleteFiles() {
+    echo "finalizando procesos"
+}
+
 #Bucle mientras que no llegue el apocalipsis
-#   -Espera un segundo
+while [ ! -f "$apocalipisFile" ]
+    do
+        sleep 1s;
+        #finish process of files
+        finishProcess
+        deleteFiles
+        echo "waiting...";
+    done
+
+rm $apocalipisFile
+
 #   -Lee las listas y revive los procesos cuando sea necaario dejando entradas en la biblia
 #   -Puede usar todos los ficheros temporales que quiera pero luego en el Apocalipsis hay que borrarlos
 #   -Hay que usar un lock para no acceder a las listas a la vez que Fausto
 #   -Ojo al cerrar los proceos, hay que terminar el arbol completo no sólo uno de ellos
 #Fin bucle
-
 #Apocalipsis: termino todos los procesos y limpio todo dejando sólo Fausto, el Demonio y la Biblia
    
--- DyASO_PED1_Olmos_Bernal_Wilder/Trabajo1/Fausto.sh
+++ DyASO_PED1_Olmos_Bernal_Wilder/Trabajo1/Fausto.sh
@@ -1,7 +1,216 @@
 #!/bin/bash
 
-#Recibe órdenes creando los procesos y listas adecuadas
+#declaración de constantes
+processFile="procesos";
+serviceProcessFile="procesos_servicio";
+intervalProcessFile="procesos_periodicos";
+devilProcessFile="procesos_demonio";
+bibleFile="Biblia.txt";
+apocalipisFile="Apocalipsis";
+saintPeterFile="SanPedro";
+files=($processFile $serviceProcessFile $intervalProcessFile $devilProcessFile $bibleFile $apocalipisFile $saintPeterFile);
+filesProcess=($processFile $serviceProcessFile $intervalProcessFile);
+hellDirectory="Infierno"
+
+# texto de ayuda
+helpText="NAME | NOMBRE\n
+    fausto.sh \n
+    \t EN: Process handler created to obtain the max punctuation \n
+    \t ES: Manejador de procesos creado para obtener la máxima calificación \n \n
+    \t SYNOPSIS | DESCRIPCIÓN \n
+    \t \t     fausto.hs [ OPTION ] [ INTERVAL ]* [ PID ]* [ COMMAND ]*\n
+    \t \t     *optional | opcional \n \n
+    \t OPTION | OPCIONES \n
+    \t \t     run [COMMAND]: to run once a simple command | para lanzar un comando una sola vez\n
+    \t \t     run-service [COMMAND]:to run a service | para lanzar un servicio \n
+    \t \t     run-periodic [INTERVAL] [COMMAND]: to run a command just for a interval time | para lanzar un comando durante un intervalo de tiempo \n
+    \t \t     help: visualize the options | visualiza las opciones \n
+    \t \t     stop: [PID] to stop a process | para parar un proceso \n
+    \t \t     end: to stop all process | para parar todos los procesos \n \n
+
+    \t EXAMPLES | EJEMPLOS \n
+    \t \t     $ ./Fausto.sh run 'sleep 10; echo hola' \n
+
+    \t \t     $ ./Fausto.sh run-service 'yes > /dev/null'\n
+       
+    \t \t     $ ./Fausto.sh run-periodic 10 'echo hola'\n
+    
+
+    \n\nAUTHOR | AUTOR \t
+        Written by Wilder Olmos Bernal
+"
+
+#Borrado de ficheros si existen
+deleteAndCreateInitialFiles() {
+    for file in ${files[@]}; do
+        #Borramos si existe el fichero
+        if [ -f "$file" ]; then
+            rm "$file";
+        fi
+        #creamos todo de nuevo menos Apocalipsis
+        if [ $file != $apocalipisFile ]; then
+            touch $file;
+        fi
+    done
+
+    #borramos y creamos el directorio Infierno.
+    if [ -d "$hellDirectory" ]; then
+        rm -Rf "$hellDirectory";
+    fi
+    mkdir $hellDirectory;
+}
+
+
+function writeBible() {
+    date=$(date +%H:%M:%S);
+    echo "${date} $1" >> $bibleFile;
+}
+
+function initBible(){
+    writeBible "---------------Génesis---------------"
+}
+
+
+function writeInProcess() {
+    echo $1 >> $processFile;
+}
+
+function writeInServiceProcess() {
+    echo $1 >> $serviceProcessFile;
+}
+
+function writeInPeriodicProcess() {
+    echo $1 >> $intervalProcessFile
+}
+
 #Si el Demonio no está vivo lo crea
+processRunning="$(ps gl)";
+isDaemonRunning=$(grep -c 'Demonio.sh' "$processRunning")
+if [ -z "$isDaemonRunning" ]
+then
+    echo "el demonio está apagado o fuera de cobertura en este momento";
+    echo "Reiniciando ficheros";
+    deleteAndCreateInitialFiles
+    echo "arrancando demonio";
+    $(nohup sh ./Demonio.sh >> /dev/null  &) 
+    initBible;
+fi
+echo $isDaemonRunning
+
+#ejecuta el comando run
+function executeRun() {
+    command=$@;
+    #ejecuta el comando y obtener el pid
+    eval $@ & PID=$!;
+    #apunta entrada en la lista de procesos
+    writeInProcess "$PID '$command'";
+    #apunta entrada en la Biblia.
+    writeBible "El proceso $PID '$command' ha nacido.";
+}
+
+#ejecuta el comando run-service
+function executeRunService() {
+    command=$@;
+    #ejecuta el comando y obtener el pid
+    eval $@ & PID=$!;
+    #apunta entrada en la lista de procesos_servicio
+    writeInServiceProcess "$PID '$command'";
+    #apunta entrada en la Biblia.
+    writeBible "El proceso $PID '$command' ha nacido.";
+}
+
+#ejecuta el comando run-periodic
+function executeRunPeriodic() {
+    interval=$1;
+    shift 1;
+    command=$@;
+    #ejecuta el comando y obtener el pid
+    eval $command & PID=$!;
+    #apunta entrada en la lista de procesos periódicos
+    writeInPeriodicProcess "0 $interval $PID '$command'";
+    #apunta entrada en la Biblia.
+    writeBible "El proceso $PID '$command' ha nacido.";
+}
+
+#ejecuta el comando list
+function executeList() {
+    echo "Procesos:";
+    $(cat $processFile);
+    echo "Procesos servicio:";
+    $(cat $serviceProcessFile);
+    echo "Procesos periódicos:";
+    $(cat $intervalProcessFile);
+}
+
+
+#ejecuta el comando help
+function executeHelp() {
+    echo -e $helpText;
+}
+
+#busca un pid en todos los ficheros
+#devuelve true si lo encuentra y false en otro caso.
+
+function findProccessInFile(){
+len=${#filesProcess[@]};
+start=0;
+result=0;
+while  [ $start -lt $len ] || [ $result -ne "" ]
+    do
+        processFile=${filesProcess[$start]};
+        start=$((start+1));
+        while IFS= read -r line
+        do
+            #arrIN=(${line//' '/ });
+            #echo ${arrIN[0]} ;
+            case $line in (*"$1"*)
+                result=$1;
+            ;;esac
+        done < $processFile;
+    done;
+ $result;
+}
+
+#ejecuta el comando stop
+function executeStop() {
+result=$(findProccessInFile $1)
+echo $result;
+    if [ $result ]; then
+        $(touch "$hellDirectory/$1");
+    fi
+}
+
+function executeEnd() {
+    touch $apocalipisFile
+}
+
+case $1 in
+  "run")
+    executeRun $2;
+  ;;
+  "run-service")
+    executeRunService $2;
+  ;;
+  "run-periodic")
+    executeRunPeriodic $2 $3;
+  ;;
+    "list")
+    executeList;
+  ;;
+    "help")
+    executeHelp;
+  ;;  
+  "stop")
+    executeStop $2;
+  ;;
+    "end")
+    executeEnd;
+  ;;
+# error
+  *)
+    echo -e 'There is and error | tienes un error /n You can use ./Fausto.sh help | puedes usar /.Fausto.sh help';
+  ;;
+esac
+
+
 #Al leer/escribir en las listas hay que usar bloqueo para no coincidir con el Demonio
-
-
